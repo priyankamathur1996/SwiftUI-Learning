6b9571017a208c5eecc879b9b86c971649458a35
priyanka.m3 <priyanka.m3@glowtouch.com>
2024-06-03 17:42:02 +0530

    Commit 1

---
+++ MovieApp/MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Priyanka Mathur on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
